cff2ed39a6d261fbab337376363456a8f2713649
Yudha <yudha.e.saputra.onwork@gmail.com>
2024-08-19 13:30:22 +0800

    Update lib.rs

---
+++ lib.rs
@@ -0,0 +1,29 @@
+use anchor_lang::prelude::*;
+
+declare_id!("YourProgramIDHere");
+
+#[program]
+pub mod my_social_nft {
+    use super::*;
+
+    pub fn create_nft(ctx: Context<CreateNft>, name: String, uri: String) -> Result<()> {
+        let nft = &mut ctx.accounts.nft;
+        nft.name = name;
+        nft.uri = uri;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct CreateNft<'info> {
+    #[account(init)]
+    pub nft: ProgramAccount<'info, Nft>,
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Nft {
+    pub name: String,
+    pub uri: String,
+}
